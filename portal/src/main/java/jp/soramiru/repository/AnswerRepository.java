b17f5ddf33e27f779686c23c29cdd6e78618afb0
tuyendang90 <dangkimtuyen1990@gmail.com>
2019-11-15 16:09:52 +0700

    Init comment

---
+++ portal/src/main/java/jp/soramiru/repository/AnswerRepository.java
@@ -0,0 +1,12 @@
+package jp.soramiru.repository;
+
+import jp.soramiru.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestionId(Long questionId);
+}
---
+++ portal/src/main/java/jp/soramiru/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package jp.soramiru.repository;
+
+import jp.soramiru.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
